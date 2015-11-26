84fe93f47c0612b893177bec2039168dc26873b7
mm <m>
2015-11-26 17:42:46 +0600

    MobileOperator information
    GP,Robi,Blink

---
+++ BdMobileOperatorsShake/app/src/main/java/com/appstech/mamun/bdmobileoperatorsshake/RobiPackageinformatins.java
@@ -0,0 +1,49 @@
+package com.appstech.mamun.bdmobileoperatorsshake;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class RobiPackageinformatins extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.robi_packageinformatins);
+
+        Button damalsamal =(Button) findViewById(R.id.damalsamal22);
+        damalsamal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent damalsamal22 = new Intent(RobiPackageinformatins.this, RobiDamalSamal.class);
+                startActivity(damalsamal22);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_robi_packageinformatins, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
